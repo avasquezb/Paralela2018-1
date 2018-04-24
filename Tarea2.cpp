90e1f3a34fb9ee6cea4e23eef3220e2691752b5c
avasquezb <tito.vasquez7@gmail.com>
2018-04-23 21:40:53 -0300

    tarea 2

---
+++ Tarea2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <xlsxwriter.h>
+#include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NOM_ENTRADA  "utem.csv" //nombre de fichero
+
+using namespace std;
+
+int ContarLineas(char archivo[]){
+	ifstream file(archivo);
+	string linea;
+	int lin=0;
+		
+		while(getline(file,linea)){
+			lin++;
+		}
+		
+		return lin;
+}
+
+int main(int argc, char* argv[]){
+
+	int CantTot=0; //cantidad de lineas
+
+	CantTot = ContarLineas(argv[1]);
+	
+	cout<<"Hay "<<CantTot-1<<" lineas en total"<<endl;
+
+	return 0;
+}
